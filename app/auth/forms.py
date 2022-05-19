84998151acc4635007ecc7cac2fdaab961db48ae
Develper-Gitonga <aizensalim52@gmail.com>
2022-05-19 15:57:23 +0300

    forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,3 +5,17 @@ from wtforms import StringField, PasswordField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired,Email,EqualTo,ValidationError
 from flask_login import current_user
 from app.models import User
+
+#Register Form 
+class Register(FlaskForm):
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("That email is taken. Please choose a different one")
+    
+    name = StringField("Name", validators=[DataRequired()],render_kw={"placeholder": "Full Name"})
+    email = StringField("Email", validators=[DataRequired(), Email()],render_kw={"placeholder": "Email"})
+    password = PasswordField("Password", validators=[DataRequired()],render_kw={"placeholder": "**********"})
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')],render_kw={"placeholder": "Confirm Password"})
+    submit = SubmitField("Sign Up")
