e2d03411bfbb872d197ebbf2c26f2d8ecb46bb1b
Jeff <Xavierjeff451@gmail.com>
2022-05-19 09:41:51 +0300

    commit

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import main
+from .forms import ReviewsForm
 
 @main.route('/')
 def index():
@@ -8,3 +9,11 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template("index.html")
+
+@main.route("/review")
+def new_review():
+    '''
+       Function to add a review
+    '''
+    form = ReviewsForm()
+    return render_template('review.html',form = form)
